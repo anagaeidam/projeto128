5d6d87c376bcc04a533e3290d5dd36a6918863b8
anagaeidam <111796810+anagaeidam@users.noreply.github.com>
2023-05-08 17:21:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWrist = 0;
+rightWrist = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+function draw() {
+    image(video,0,0,600,500);
+}
+
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWrist = results[0].pose.rightWrist.x;
+        rightWrist = results[0].pose.rightWrist.y;
+        console.lpg("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
